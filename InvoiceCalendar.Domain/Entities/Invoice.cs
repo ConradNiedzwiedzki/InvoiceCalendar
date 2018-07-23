eebf1cc814467906b8dd16b326321dac60fc9e0e
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-07-23 18:52:00 +0200

    Added imitation of implementation in NinjectDependecyResolver

--- InvoiceCalendar.Domain/Entities/Invoice.cs
+++ InvoiceCalendar.Domain/Entities/Invoice.cs
@@ -5,7 +5,6 @@ namespace InvoiceCalendar.Domain.Entities
     public class Invoice
     {
         public int InvoiceID { get; set; }
-        public string Name { get; set; }
         public string Company { get; set; }
         public decimal Price { get; set; }
         public DateTime Date { get; set; }
--- InvoiceCalendar.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ InvoiceCalendar.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -1,4 +1,7 @@
-﻿using Ninject;
+﻿using InvoiceCalendar.Domain.Abstract;
+using InvoiceCalendar.Domain.Entities;
+using Moq;
+using Ninject;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -29,7 +32,13 @@ namespace InvoiceCalendar.WebUI.Infrastructure
         
         private void AddBindings()
         {
-            //additional bindings
+            Mock<IInvoiceRepository> mock = new Mock<IInvoiceRepository>();
+            mock.Setup(m => m.Invoices).Returns(new List<Invoice> {
+                new Invoice { Company = "KMD", Price = 1100, Date = DateTime.Today },
+                new Invoice { Company = "DPD", Price = 800, Date = DateTime.Today },
+                new Invoice { Company = "Paxer", Price = 350, Date = DateTime.Today }
+            });
+            _kernel.Bind<IInvoiceRepository>().ToConstant(mock.Object);
         }
     }
 }
