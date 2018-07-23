d8160446358ec052ad177f5107281d0db9fff9a7
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-07-23 18:58:22 +0200

    Added Invoice controller

---
+++ InvoiceCalendar.WebUI/Controllers/InvoiceController.cs
@@ -0,0 +1,23 @@
+﻿using InvoiceCalendar.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace InvoiceCalendar.WebUI.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        private IInvoiceRepository _repository;
+        public InvoiceController(IInvoiceRepository invoiceRepository)
+        {
+            this._repository = invoiceRepository;
+        }
+
+        public ViewResult List()
+        {
+            return View(_repository.Invoices);
+        }
+    }
+}
