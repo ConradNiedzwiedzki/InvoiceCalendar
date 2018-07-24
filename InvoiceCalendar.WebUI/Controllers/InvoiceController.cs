c239537ca749ca1af0b5e243bdcd9bbdce92138c
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-07-24 22:09:58 +0200

    Added pagination unit test

--- InvoiceCalendar.WebUI/Controllers/InvoiceController.cs
+++ InvoiceCalendar.WebUI/Controllers/InvoiceController.cs
@@ -10,14 +10,18 @@ namespace InvoiceCalendar.WebUI.Controllers
     public class InvoiceController : Controller
     {
         private IInvoiceRepository _repository;
+        public int PageSize = 4;
         public InvoiceController(IInvoiceRepository invoiceRepository)
         {
             this._repository = invoiceRepository;
         }
 
-        public ViewResult List()
+        public ViewResult List(int page = 1)
         {
-            return View(_repository.Invoices);
+            return View(_repository.Invoices
+            .OrderBy(p => p.InvoiceID)
+            .Skip((page - 1) * PageSize)
+            .Take(PageSize));
         }
     }
 }
