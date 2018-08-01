d25b2445dc7003ba74e62445dfd470c72aa9ef41
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-08-01 23:34:52 +0200

    Typos

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -9,12 +9,16 @@ namespace InvoiceCalendar.Models
     public class Invoice
     {
         public int ID { get; set; }
+
+        [Display(Name = "Nazwa firmy")]
         public string Company { get; set; }
+
+        [Display(Name = "Kwota faktury")]
         public double Price { get; set; }
 
-        [Display(Name = "Created Date")]
+        [Display(Name = "Data faktury")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime CreatedDate { get; set; }
     }
 }
