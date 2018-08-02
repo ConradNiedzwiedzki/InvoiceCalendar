174bf28271f4bf2c7b1ff2737c1f93497c04656a
Conrad Niedzwiedzki <kontakt@conradniedzwiedzki.pl>
2018-08-02 22:44:59 +0200

    Added small validation

--- Models/Invoice.cs
+++ Models/Invoice.cs
@@ -10,12 +10,16 @@ namespace InvoiceCalendar.Models
     {
         public int ID { get; set; }
 
+        [Required]
         [Display(Name = "Nazwa firmy")]
         public string Company { get; set; }
 
+        [Required]
         [Display(Name = "Kwota faktury")]
         public double Price { get; set; }
 
+        [Required]
+        [Range(0,10000000)]
         [Display(Name = "Data faktury")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
